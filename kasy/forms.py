8d4e7c059bc05301c3707316c8cdb1ab4e1b5600
Ireneusz Stalicki <istalicki@gmail.com>
2019-05-21 12:59:12 +0200

    zmaina pola input data_fisk i ostatni_prze we wprowadzaniu kasy na typ date

--- kasy/forms.py
+++ kasy/forms.py
@@ -2,11 +2,18 @@ from django import forms
 from .models import Kasa, Podatnik
 
 
+class DateInput(forms.DateInput):
+	input_type = 'date'
+
 class KasaForm(forms.ModelForm):
 
     class Meta:
         model = Kasa
         fields = '__all__'
+        widgets = {
+        	'data_fisk' : DateInput(),
+        	'ostatni_przeg' : DateInput(),
+        }
 
 
 class PodatnikForm(forms.ModelForm):
