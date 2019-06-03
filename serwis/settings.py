189aa6cc59d626008f8d5d850330cc12a12bdc4f
I.Stalicki <ireks@op.onet.pl>
2019-06-03 22:12:10 +0200

    ff

--- serwis/settings.py
+++ serwis/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'OPTIONS': {
+#                'host' : '192.168.0.13'
 #             'read_default_file': '/etc/mysql/my.cnf',
 #         }
 #     }
